c990befb154741e0fcffcd1d938ef0c1f75f0756
christina <christinakimario8@gmail.com>
2025-08-08 15:39:03 +0300

    requirements

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+Post.objects.filter(published_date_lte=timezone.now()).order
